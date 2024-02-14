22a2bede7f0871728cf5dfab0e8a699545a681bc
Guilherme Serpa Batista <serpa419@gmail.com>
2024-02-14 13:20:15 -0300

    update text

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">FENEG 2024 - Feira de Negócios Sicoob Frutal.</h1>
           <p className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
             Sua porta para o futuro dos negócios em Frutal!
-            Descubra oportunidades, faça networking e impulse seu crescimento.
+            Descubra oportunidades, faça networking e impulsione seu crescimento.
           </p>
         </div>
         <div className="flex justify-center align-middle items-center">

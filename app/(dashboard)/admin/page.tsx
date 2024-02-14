13f943ed86eac82bfb9e15149717887bb8fa1774
Guilherme Serpa Batista <serpa419@gmail.com>
2024-02-14 14:38:14 -0300

    update "charts"

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,13 +1,38 @@
 'use client';
 import Login from '@/components/admin/login'
+import LoadingError from '@/components/error-loading';
+import Loading from '@/components/loading';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useSession } from 'next-auth/react';
+import useSWR from 'swr';
 
 
 export default function Dashboard() {
   const session = useSession();
-  return (
-    <div className="flex grow flex-col justify-center align-middle items-center h-full">
-      {session.status === 'authenticated' ? (<pre>{session?.data?.user?.email}</pre>) : <Login />}
-    </div>
-  )
+  const { data, error, isLoading } = useSWR('/api/admin/registers')
+  if (session.status === 'unauthenticated') {
+    return (
+      <div className="flex grow flex-col justify-center align-middle items-center h-full">
+        <Login />
+      </div>
+    )
+  } else if (session.status === 'authenticated') {
+    if (error) return <LoadingError />
+    if (isLoading) return <Loading />
+    return (
+      <div className="flex grow flex-col h-full justify-between">
+        <Card className='w-fit m-5'>
+          <CardHeader>
+            <CardTitle>
+              Número de Registros.
+            </CardTitle>
+            <CardContent className='flex items-center justify-center align-middle h-full'>
+              <p className='flex text-5xl shadow-lg font-semibold justify-center'>{data.length}</p>
+            </CardContent>
+          </CardHeader>
+        </Card>
+      </div>
+    )
+  }
+
 }

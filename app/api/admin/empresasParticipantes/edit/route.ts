8c66c5cb469f2c50bd51fad1c33723985f7aeb1f
Guilherme Serpa Batista <serpa419@gmail.com>
2024-02-24 17:47:51 -0300

    edit update

---
+++ app/api/admin/empresasParticipantes/edit/route.ts
@@ -0,0 +1,47 @@
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/lib/authOptions"
+import prisma from "@/lib/prisma"
+
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDNARY_CLOUD_NAME,
+    api_key: process.env.CLOUDNARY_API_KEY,
+    api_secret: process.env.CLOUDNARY_API_SECRET
+});
+
+export async function PUT(req: Request) {
+    const session = await getServerSession(authOptions)
+    if (!session) {
+        return new Response('Não autorizado!', { status: 401 })
+    }
+    try {
+        const data = await req.json()
+        const deleteImage = await cloudinary.uploader.destroy(data.newValues.public_id)
+
+        const res = await prisma.empresas.update({
+            where: {
+                id: data.newValues.id
+            },
+            data: {
+                nome: data.newValues.nome,
+                email: data.newValues.email,
+                instagram: data.newValues.instagram,
+                telefone: data.newValues.telefone,
+                whatsapp: data.newValues.whatsapp,
+                endereco: data.newValues.endereco,
+                cidade: data.newValues.cidade,
+                estado: data.newValues.estado,
+                public_id: data.newPublicId,
+                url: data.newUrl,
+
+            }
+        })
+
+        return new Response(JSON.stringify(res), { status: 200 })
+    } catch (error) {
+        console.log(error);
+
+        return new Response(JSON.stringify(error), { status: 500 })
+    }
+}

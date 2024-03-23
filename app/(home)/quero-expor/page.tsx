3a1146d471996d397f70b88b1d94e33390a45dfc
Guilherme Serpa Batista <serpa419@gmail.com>
2024-03-23 11:55:23 -0300

    layout update

--- app/(home)/quero-expor/page.tsx
+++ app/(home)/quero-expor/page.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import useSWR from 'swr'
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { FileDown } from 'lucide-react';
 
 export default function Expositor() {
     const { data, isLoading, error } = useSWR('/api/queroExpor')
@@ -13,7 +14,7 @@ export default function Expositor() {
     return (
         <div className='flex justify-center flex-wrap'>
             {/* <h1 className='flex mb-4 justify-center items-center text-4xl font-extrabold leading-none tracking-tight text-verde-escuro md:text-5xl lg:text-6xl dark:text-white m-5'>QUERO SER EXPOSITOR</h1> */}
-            <div className='flex flex-col justify-center w-2/4'>
+            <div className='flex flex-col justify-center w-2/4 min-w-[400px]'>
                 <div className='flex justify-center pt-5'>
                     <Image src={'https://res.cloudinary.com/dt8o4rsmt/image/upload/v1709146059/sobre-nos/kf1cioeh4wtdtcjibhan.png'} width={410} height={180} alt='Logo Quero Expor' />
                 </div>
@@ -21,16 +22,19 @@ export default function Expositor() {
                     <FormExpo />
                 </div>
             </div>
+
             {data &&
-                <div className='flex flex-col items-center w-2/4 space-y-2'>
-                    <h1 className='text-2xl text-verde-escuro font-bold p-5 pb-0'>Informações</h1>
-                    <Button><Link href={data.url_pdf ?? ''} rel="noopener noreferrer" target="_blank"> Apresentação </Link></Button>
-                    <img src={data.url_image} alt='Mapa' className='px-16 rounded-2xl' />
-                    <video controls autoPlay muted className='px-16'>
-                        <source src={data.url_video} type="video/mp4" />
-                        Seu navegador não suporta a execução de videos.
-                    </video>
-                </div>
+                <>
+                    <div className='flex flex-col items-center w-2/4 space-y-2 min-w-[450px]'>
+                        <h1 className='text-2xl text-verde-escuro font-bold p-5 pb-0'>Mapa do Evento</h1>
+                        <Button><Link href={data.url_pdf ?? ''} rel="noopener noreferrer" target="_blank">Apresentação </Link><FileDown /></Button>
+                        <img src={data.url_image} alt='Mapa' className='px-16 rounded-2xl' />
+                        <video controls autoPlay muted className='px-16 pb-5'>
+                            <source src={data.url_video} type="video/mp4" />
+                            Seu navegador não suporta a execução de videos.
+                        </video>
+                    </div>
+                </>
             }
         </div>
     )

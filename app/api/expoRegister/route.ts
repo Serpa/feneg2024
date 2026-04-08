a7358da32bc713cf905b436cd4ae188b59defb99
Guilherme Serpa Batista <serpa419@gmail.com>
2026-04-08 08:52:59 -0300

    Remove 2025

--- app/api/expoRegister/route.ts
+++ app/api/expoRegister/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Novo Interesse em Exposição - FENEG 2025</title>
+  <title>Novo Interesse em Exposição - FENEG</title>
   <style>
     body {
       font-family: Arial, sans-serif;
